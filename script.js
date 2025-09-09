2bd48d172a8a713d54f6f5c1188eb15af922e8cc
dinnerboneA <ahmedfaisal1255@gmail.com>
2025-09-10 00:50:39 +0800

    Initial commit from vs code

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navlinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navlinks.classList.toggle('active');
+    
+}
+
+
+
